15ec3cac4342a3713934a09f3a8bec4f2b9265db
Ahmed Zubair <153666614+AhmedZubair18@users.noreply.github.com>
2024-12-19 01:38:39 +0500

    General question

---
+++ app.js
@@ -0,0 +1,33 @@
+function plus(){
+   let show = document.querySelector(".hide").style.display = "flex"
+   let plus = document.querySelector(".plus").style.display = "none"
+   let minus = document.querySelector(".minus").style.display = "flex"
+}
+
+function minus(){
+    let hide = document.querySelector(".hide").style.display = "none"
+    let minus = document.querySelector(".minus").style.display = "none"
+    let plus = document.querySelector(".plus").style.display = "flex"
+}
+function plusone(){
+    let show = document.querySelector(".hide-1").style.display = "flex"
+    let plus = document.querySelector(".plusone").style.display = "none"
+    let minus = document.querySelector(".minusone").style.display = "flex"
+ }
+ 
+ function minusone(){
+     let hide = document.querySelector(".hide-1").style.display = "none"
+     let minus = document.querySelector(".minusone").style.display = "none"
+     let plus = document.querySelector(".plusone").style.display = "flex"
+ }
+ function plustwo(){
+    let show = document.querySelector(".hide-3").style.display = "flex"
+    let plus = document.querySelector(".plustwo").style.display = "none"
+    let minus = document.querySelector(".minustwo").style.display = "flex"
+ }
+ 
+ function minustwo(){
+     let hide = document.querySelector(".hide-3").style.display = "none"
+     let minus = document.querySelector(".minustwo").style.display = "none"
+     let plus = document.querySelector(".plustwo").style.display = "flex"
+ }
